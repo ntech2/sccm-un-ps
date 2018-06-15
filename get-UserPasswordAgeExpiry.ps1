3240077793a530a349004c89d1beca01c8b65072
ntech2 <wackers727@gmail.com>
2018-06-15 11:39:56 +0300

    fdas

---
+++ get-UserPasswordAgeExpiry.ps1
@@ -0,0 +1,52 @@
+﻿$date = Get-Date -format dd.MM.yyyy.
+Import-Module ActiveDirectory
+<#
+$Currentuser =Get-WmiObject –Class Win32_ComputerSystem | Select-Object UserName
+$Currentuser = $Currentuser | Select-Object username -ExpandProperty *
+$Currentuser = $Currentuser.split("\")
+$Currentuser = $Currentuser[1]
+$Currentuser = "grebezse" #>
+
+$users = "grebezse","livmanir","samsoe01","liepid02","baronsr","zirnovsa","kukulei","jurkoa01","zebergsa"
+cls
+foreach($user in $users){
+
+$user = get-aduser -identity $user -Properties *|select Name,SamAccountName,PasswordLastSet,PasswordNeverExpires,PasswordExpired,Memberof
+$name = $user.samaccountname
+$maxPasswordAge = (Get-ADDefaultDomainPasswordPolicy).MaxPasswordAge
+$passwordsetdate = $user.PasswordLastSet
+
+$oneyear = $false
+$memberof = $user.memberof
+  $memberof |foreach { if ($_ -match "FGP_VienuGaduParole"){[timespan]$maxPasswordAge = "365.00:00:00"}}
+  
+
+    $expireson = $passwordsetdate + $maxPasswordAge
+    $today = $date
+    $daystoexpire = (New-TimeSpan -Start $today -End $Expireson).Days
+        
+    
+    $messageDays = $daystoexpire
+
+    if (($messageDays) -ge "1")
+    {
+        $messageDays = "pēc " + "$daystoexpire" + " dienām(-s)."
+    }
+    elseif((($messageDays) -lt "0") -and ($user.PasswordNeverExpires -eq $false))
+    {
+        $messageDays = "beigusies"
+    }
+    elseif((($messageDays) -lt "0") -and ($user.PasswordNeverExpires -eq $true))
+    {
+        $messageDays = "nekad"
+    }
+    else
+    {
+        $messageDays = "šodien"
+    }
+    $name = $user.samaccountname
+    Write-Host "Parole lietotājam $name beidzas: $messageDays - $expireson"
+    }
+
+
+  
---
+++ get_ad_comp_container_names_lastlogons.ps1
@@ -0,0 +1,14 @@
+﻿$pcs = Get-Content -Path "C:\temp\compContPCLIST.txt"
+
+foreach ($pc in $pcs)
+{
+write-host "Trying $pc"
+Get-WmiObject -Class Win32_NetworkLoginProfile -ComputerName $pc | 
+Sort-Object -Property LastLogon -Descending | 
+Select-Object -Property * -First 1 | 
+Where-Object {$_.LastLogon -match "(\d{14})"} | 
+Foreach-Object { New-Object PSObject -Property @{ Name=$_.Name;LastLogon=[datetime]::ParseExact($matches[0], "yyyyMMddHHmmss", $null)}} | Export-Csv -Path c:\temp\compContainerReport.csv -Append
+write-host "$pc donezo!"
+}
+
+
